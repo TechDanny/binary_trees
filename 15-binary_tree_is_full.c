944ce29a369b8b78302862b3d4cfe25d59cd1950
TechDanny <kokidanny15@gmail.com>
2023-08-30 06:38:37 -0700

    check if tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,14 +13,14 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if ((tree->left == NULL && tree->right == NULL) || (tree->left != NULL
-			&& tree->right != NULL))
+	if (tree->left == NULL && tree->right == NULL)
 	{
-		return (binary_tree_is_full(tree->left) &&
-				binary_tree_is_full(tree->right));
+			return (1);
 	}
-	else
+	if (tree->left != NULL && tree->right != NULL)
 	{
-		return (0);
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(
+					tree->right));
 	}
+	return (0);
 }
