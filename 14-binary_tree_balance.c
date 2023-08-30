3501325a9d942a0840a3097790ea43bc9cab5a23
TechDanny <kokidanny15@gmail.com>
2023-08-30 05:06:17 -0700

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -40,14 +40,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree->left)
-	{
-		leftHeight += 1 + binary_tree_height(tree->left);
-	}
-	if (tree->right)
-	{
-		rightHeight += 1 + binary_tree_height(tree->right);
-	}
+	leftHeight += 1 + binary_tree_height(tree->left);
+	rightHeight += 1 + binary_tree_height(tree->right);
 
 	return (leftHeight > rightHeight ? leftHeight : rightHeight);
 }
